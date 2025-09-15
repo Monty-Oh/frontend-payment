b137837a97e1dd7ca5a4ecf73a4218db3ee195dc
Monty Oh <ohhanjin0330@gmail.com>
2025-09-15 10:25:33 +0900

    결제취소 수정 중

--- routes/payment.js
+++ routes/payment.js
@@ -22,33 +22,46 @@ router.get('/inisys/result/close', (req, res) => {
 
 //  이니시스 인증 결과 수신
 router.post('/inisys/result/authentication', async (req, res) => {
+    //  다음 URL
+    let nextPath = path.join(__dirname, '..', 'public', 'views', 'inisys', 'success.html');
+
     if (req.body && req.body.resultCode === "0000") {
         const requestBody = req.body;
+        const merchantData = _decodeMerchantData(requestBody.merchantData);
         const requestBodySavePayment = {
             mid: requestBody.mid,
             orderNo: requestBody.orderNumber,
             authToken: requestBody.authToken,
             idcCode: requestBody.idc_name,
             approvalUrl: requestBody.authUrl,
-            cancelUrl: requestBody.netCancelUrl
+            networkCancelUrl: requestBody.netCancelUrl,
+            price: merchantData.price
         }
         const responseCreatePayment = await api.post('http://localhost:3000/payment/v1/payments/inicis', requestBodySavePayment);
         if (responseCreatePayment) {
             if (responseCreatePayment.status === 200 && responseCreatePayment.data && responseCreatePayment.data.paymentNo) {
-                const responseApprovePayment = await api.post(`http://localhost:3000/payment/v1/payments/inicis/${responseCreatePayment.data.paymentNo}/approval`)
-                debugger
+                const responseApprovalPayment = await api.post(`http://localhost:3000/payment/v1/payments/${responseCreatePayment.data.paymentNo}/approval`);
+                if (!responseApprovalPayment || responseApprovalPayment.status !== 200) {
+                    nextPath = path.join(__dirname, '..', 'public', 'views', 'inisys', 'error.html');
+                }
             } else {
-
+                nextPath = path.join(__dirname, '..', 'public', 'views', 'inisys', 'error.html');
             }
         }
-        // if (responseCreatePayment) {
-        //
-        // }
-        // const responseApprovePayment = await api.post(`http://localhost:3000/payment/v1/payments/inicis/${responseCreatePayment.paymentNo}/approval`)
-        console.log(responseCreatePayment);
+    } else {
+        nextPath = path.join(__dirname, '..', 'public', 'views', 'inisys', 'error.html');
     }
-
-    console.log(req.body);
+    res.sendFile(nextPath);
 });
+
+const _decodeMerchantData = function (encoded) {
+    // Base64 → 문자열 (중간에 UTF-8 디코딩 필요)
+    const decodedStr = new TextDecoder().decode(
+        Uint8Array.from(atob(encoded), c => c.charCodeAt(0))
+    );
+
+    // 문자열 → 객체
+    return JSON.parse(decodedStr);
+}
 
 module.exports = router;
